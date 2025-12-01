6ca6664479957caa9353e7d852702cca51263812
Chamano Ramashia <chamano.sr@gmail.com>
2025-12-01 11:43:48 +0200

    designing the projects layout

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,6 +4,7 @@ import Sidebar from "./components/sidebar/Sidebar";
 import LandingPage from "./components/landingpage/LandingPage";
 import Footer from "./components/footer/Footer";
 import About from "./pages/about/About";
+import Projects from "./pages/projects/Projects";
 import { Routes, Route } from "react-router-dom";
 
 export default function App() {
@@ -22,6 +23,7 @@ export default function App() {
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
           </Routes>
 
           <Footer />
